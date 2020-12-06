e70edc2ce62ef8608a1e5d60a4f85a89c7adeaf6
michaelnoel <noel.michael87@gmail.com>
2020-12-05 18:18:17 -0600

    im_plot1 choice 4 created

--- sections/implementation/im_script.R
+++ sections/implementation/im_script.R
@@ -9,8 +9,10 @@
 im_plot1_choice1 <- '1. Visualization of Core Creative Monthly Income from Jan 2013 to October 2020'
 im_plot1_choice2 <- '2. Income v Time - Idenficication of Outlier'
 im_plot1_choice3 <- '3. Income v Time - w/o Outlier'
+im_plot1_choice4 <- '4. Visualization of Core Creative Monthly Income from Jan 2012 to October 2020 w/o Outlier'
+
 im_plot1_choices <- list(
-  im_plot1_choice1, im_plot1_choice2, im_plot1_choice3
+  im_plot1_choice1, im_plot1_choice2, im_plot1_choice3, im_plot1_choice4
 )
 
 # im_render_plot1_choice1 Time_Series_Analysis.R Line 2-6
@@ -32,6 +34,14 @@ im_render_plot1_choice2 <- function(dataframe) {
 im_render_plot1_choice3 <- function(dataframe) {
   dataframe2 <- dataframe[-64,]
   t2 <- seq(1, 93)
+  plot <- plot(dataframe2$income ~ t2, ylab = "Monthly Income in Dollars", xlab = "Time")
+  plot
+}
+
+# im_render_plot1_choice4
+im_render_plot1_choice4 <- function(dataframe) {
+  dataframe2 <- dataframe[-64,]
+  t2 <- seq(1, 93)
   plot <- ts.plot(dataframe2$income, ylab = "Monthly Income in Dollars", xlab = "Time")
   lines(predict(lm(dataframe2$income ~ t2)), col = 'green')
   plot
@@ -49,6 +59,10 @@ im_render_plot1 <- function(dataframe, choice) {
   }
   if (choice == im_plot1_choice3) {
     plot <- im_render_plot1_choice3(dataframe)
+    plot
+  }
+  if (choice == im_plot1_choice4) {
+    plot <- im_render_plot1_choice4(dataframe)
     plot
   }
 }
