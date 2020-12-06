61a1b2db376d807108b817c6602cb37fd908218f
michaelnoel <noel.michael87@gmail.com>
2020-12-05 21:48:33 -0600

    inclusion of all needed im_plot3 plots

--- sections/implementation/im_script.R
+++ sections/implementation/im_script.R
@@ -182,18 +182,18 @@ im_render_plot2 <- function(dataframe, choice) {
 
 # im_plot3_a Time_Series_Analysis.R Lines 2-6
 im_plot3_choice1 <- 'im_plot3_choice1'
-#im_plot3_choice2 <- 'PACF'
-#im_plot3_choice3 <- 'ACF of Income'
-#im_plot3_choice4 <- 'PACF'
-#im_plot3_choice5 <- 'ACF of Income'
-#im_plot3_choice6 <- 'PACF'
+im_plot3_choice2 <- 'im_plot3_choice2'
+im_plot3_choice3 <- 'im_plot3_choice3'
+im_plot3_choice4 <- 'im_plot3_choice4'
+im_plot3_choice5 <- 'im_plot3_choice5'
+im_plot3_choice6 <- 'im_plot3_choice6'
 
 im_plot3_choices <- list(
-  im_plot3_choice1 # im_plot3_choice2, im_plot3_choice3, im_plot3_choice4, im_plot3_choice5, im_plot3_choice6
+  im_plot3_choice1, im_plot3_choice2 , im_plot3_choice3, im_plot3_choice4, im_plot3_choice5, im_plot3_choice6
 )
 
 
-# im_render_plot3_choice1 Time_Series_Analysis.R Line 112-113
+# im_render_plot3_choice1 Time_Series_Analysis.R Line 116
 im_render_plot3_choice1 <- function(dataframe) {
   dataframe3 <- im_dataframe3(dataframe)
   plot <- ts.plot(residuals(im_lagout2(dataframe3)),
@@ -204,45 +204,70 @@ im_render_plot3_choice1 <- function(dataframe) {
   plot
 }
 
-## im_render_plot3_choice2 Time_Series_Analysis.R Line 118
-#im_render_plot3_choice2 <- function(dataframe) {
-#  dataframe2 <- dataframe[-64,]
-#  acf_plot <- acf(dataframe2$income, main = "ACF")
-#  acf_plot
-#}
-#
-## im_render_plot3_choice3 Time_Series_Analysis.R Line 114
-#im_render_plot3_choice3 <- function(dataframe) {
-#  dataframe2 <- dataframe[-64,]
-#  acf_plot <- acf(dataframe2$income, main = "ACF")
-#  acf_plot
-#}
-#
-## im_render_plot3_choice4 Time_Series_Analysis.R Line 115
-#im_render_plot3_choice4 <- function(dataframe) {
-#  dataframe2 <- dataframe[-64,]
-#  acf_plot <- acf(dataframe2$income, main = "ACF")
-#  acf_plot
-#}
-#
-## im_render_plot3_choice5 Time_Series_Analysis.R Line 116
-#im_render_plot3_choice5 <- function(dataframe) {
-#  dataframe2 <- dataframe[-64,]
-#  acf_plot <- acf(dataframe2$income, main = "ACF")
-#  acf_plot
-#}
-#
-## im_render_plot3_choice6 Time_Series_Analysis.R Line 117
-#im_render_plot3_choice6 <- function(dataframe) {
-#  dataframe2 <- dataframe[-64,]
-#  acf_plot <- acf(dataframe2$income, main = "ACF")
-#  acf_plot
-#}
+# im_render_plot3_choice2 Time_Series_Analysis.R Line 121
+im_render_plot3_choice2 <- function(dataframe) {
+  dataframe3 <- im_dataframe3(dataframe)
+  lagout2 <- im_lagout2(dataframe3)
+  plot <- plot(fitted(lagout2) ~ residuals(lagout2), xlab = "Residuals", ylab = "Fitted")
+  plot
+}
+
+# im_render_plot3_choice3 Time_Series_Analysis.R Line 117
+im_render_plot3_choice3 <- function(dataframe) {
+  dataframe3 <- im_dataframe3(dataframe)
+  lagout2 <- im_lagout2(dataframe3)
+  plot <- acf(residuals(lagout2), main = "Residuals, Autocorrelation")
+  plot
+}
+
+# im_render_plot3_choice4 Time_Series_Analysis.R Line 118
+im_render_plot3_choice4 <- function(dataframe) {
+  dataframe3 <- im_dataframe3(dataframe)
+  lagout2 <- im_lagout2(dataframe3)
+  plot <- pacf(residuals(lagout2), main = "Residuals, Partial Autocorrelation")
+  plot
+}
+
+# im_render_plot3_choice5 Time_Series_Analysis.R Line 119
+im_render_plot3_choice5 <- function(dataframe) {
+  dataframe3 <- im_dataframe3(dataframe)
+  lagout2 <- im_lagout2(dataframe3)
+  plot <- hist(residuals(lagout2), xlab = "residuals(model)", main = "Residuals")
+  plot
+}
+
+# im_render_plot3_choice6 Time_Series_Analysis.R Line 120
+im_render_plot3_choice6 <- function(dataframe) {
+  dataframe3 <- im_dataframe3(dataframe)
+  lagout2 <- im_lagout2(dataframe3)
+  plot <- qqnorm(residuals(lagout2), main = "Normal Q-Q Plot Residuals", pch = 19)
+  plot
+}
 
 # im_render_plot3
 im_render_plot3 <- function(dataframe, choice) {
   if (choice == im_plot3_choice1) {
     plot <- im_render_plot3_choice1(dataframe)
+    plot
+  }
+  if (choice == im_plot3_choice2) {
+    plot <- im_render_plot3_choice2(dataframe)
+    plot
+  }
+  if (choice == im_plot3_choice3) {
+    plot <- im_render_plot3_choice3(dataframe)
+    plot
+  }
+  if (choice == im_plot3_choice4) {
+    plot <- im_render_plot3_choice4(dataframe)
+    plot
+  }
+  if (choice == im_plot3_choice5) {
+    plot <- im_render_plot3_choice5(dataframe)
+    plot
+  }
+  if (choice == im_plot3_choice6) {
+    plot <- im_render_plot3_choice6(dataframe)
     plot
   }
 }
