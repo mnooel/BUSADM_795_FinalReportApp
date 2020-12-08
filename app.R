5e6d1c994e8839cb0127a504dd2303a38a90c9c9
michaelnoel <noel.michael87@gmail.com>
2020-12-08 09:45:02 -0600

    attempted to render neural network

--- app.R
+++ app.R
@@ -305,7 +305,7 @@ implementation <- tabPanel(
         sliderInput("im_plot9_slider2", "layer 2 neurons", 0, 10, 0, width = '100%'),
         sliderInput("im_plot9_slider3", "layer 3 neurons", 0, 10, 0, width = '100%'),
         sliderInput('im_plot9_slider4', 'test population %', 10, 90, 80, width = '100%'),
-        actionButton('go_nn', "render nn", class = 'btn-success'),
+        #actionButton('go_nn', "render nn", class = 'btn-success'),
 
 
         imageOutput(outputId = 'im_plot9', height = 1000),
@@ -482,108 +482,122 @@ server <- function(input, output, session) {
   output$im_plot9 <- renderImage({
     # plot the NN
 
+    render_neural_network <- function(layer1, layer2, layer3, percent_test) {
 
-    ### layers ###
-    layers <- list(input$im_plot9_slider1, input$im_plot9_slider2, input$im_plot9_slider3)
-    neuron_vec <- c()
 
-    for (layer in layers) {
-      if (layer == 0) { }
-      else(neuron_vec <- append(neuron_vec, layer))
+      ### layers ###
+      layers <- list(layer1, layer2, layer3)
+      neuron_vec <- c()
+
+      for (layer in layers) {
+        if (layer == 0) { }
+        else(neuron_vec <- append(neuron_vec, layer))
+      }
+      print(neuron_vec)
+
+      # test percentage
+      test_percentage <- (percent_test / 100)
+
+      ### neural network ###
+      # remove outliers
+      dataframe <- nn_data_df[-64,]
+
+      # numeric index cols for months
+      Jan <- as.numeric(dataframe$month_name == "Jan")
+      Feb <- as.numeric(dataframe$month_name == "Feb")
+      Mar <- as.numeric(dataframe$month_name == "Mar")
+      Apr <- as.numeric(dataframe$month_name == "Apr")
+      May <- as.numeric(dataframe$month_name == "May")
+      Jun <- as.numeric(dataframe$month_name == "Jun")
+      Jul <- as.numeric(dataframe$month_name == "Jul")
+      Aug <- as.numeric(dataframe$month_name == "Aug")
+      Sep <- as.numeric(dataframe$month_name == "Sep")
+      Oct <- as.numeric(dataframe$month_name == "Oct")
+      Nov <- as.numeric(dataframe$month_name == "Nov")
+      Dec <- as.numeric(dataframe$month_name == "Dec")
+      # bind to the other_data_data
+      dataframe <- cbind(dataframe, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
+
+      # other_data_data columns needed
+
+      # lag each column
+      income_1 <- Lag(dataframe$income, 1)
+      hours_worked_1 <- Lag(dataframe$hours_worked, 1)
+      billable_hours_1 <- Lag(dataframe$billable_hours, 1)
+      billable_amount_1 <- Lag(dataframe$billable_amount, 1)
+      new_biz_hours_1 <- Lag(dataframe$new_biz_hours, 1)
+      new_biz_amount_1 <- Lag(dataframe$new_biz_amount, 1)
+      num_of_time_entries_1 <- Lag(dataframe$num_of_time_entries, 1)
+      number_of_time_entries_billable_1 <- Lag(dataframe$number_of_time_entries_billable, 1)
+      number_of_new_biz_time_entries_1 <- Lag(dataframe$number_of_new_biz_time_entries, 1)
+      cost_of_goods_sold_1 <- Lag(dataframe$cost_of_goods_sold, 1)
+      expense_1 <- Lag(dataframe$expense, 1)
+
+      # new other_data_data.frame of neural network other_data_data
+      NN.data <- data.frame(dataframe$income, dataframe$month, income_1, hours_worked_1, billable_hours_1, billable_amount_1,
+                            new_biz_hours_1, new_biz_amount_1, num_of_time_entries_1,
+                            number_of_time_entries_billable_1, number_of_new_biz_time_entries_1,
+                            cost_of_goods_sold_1, expense_1, Jan, Feb, Mar, Apr,
+                            May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
+
+
+      # omit nan
+      NN.data <- na.omit(NN.data)
+
+      samplesize <- test_percentage * nrow(NN.data)
+      set.seed(80)
+
+      index <- sample(seq_len(nrow(NN.data)), size = samplesize)
+      datatrain <- NN.data[index,]
+      datatest <- NN.data[-index,]
+      max <- apply(NN.data, 2, max)
+      min <- apply(NN.data, 2, min)
+      scaled <- as.data.frame(scale(NN.data, center = min, scale = max - min))
+      trainNN <- scaled[index,]
+      trainNN
+      testNN <- scaled[-index,]
+      set.seed(80)
+
+      NN <- neuralnet(dataframe.income ~ number_of_new_biz_time_entries_1 +
+        billable_amount_1 +
+        cost_of_goods_sold_1 +
+        dataframe.month +
+        Oct +
+        Nov +
+        Feb, trainNN, hidden = neuron_vec, linear.output = T, stepmax = 1e7)
+
+
+      # adjust to height of session
+      width <- session$clientData$output_im_plot9_width
+      height <- session$clientData$output_im_plot9_height
+
+      # A temp file to save the output. It will be deleted after renderImage
+      # sends it, because deleteFile=TRUE.
+      outfile <- tempfile(fileext = '.png')
+      print(outfile)
+
+      png(outfile, width = width, height = height)
+      #dev.off()
+      print(plot(NN))
+      dev.print(png, outfile, width = width, height = height)
+      #dev.off()
+      #plot(NN, file = outfile)
+      dev.off()
+
+      print(outfile)
+
+      list(src = outfile,
+           width = width,
+           height = height,
+           alt = "Rendered Neural Network")
     }
-    print(neuron_vec)
 
-    # test percentage
-    test_percentage <- (input$im_plot9_slider4 / 100)
+    #input$go_nn
 
-    ### neural network ###
-    # remove outliers
-    dataframe <- nn_data_df[-64,]
-
-    # numeric index cols for months
-    Jan <- as.numeric(dataframe$month_name == "Jan")
-    Feb <- as.numeric(dataframe$month_name == "Feb")
-    Mar <- as.numeric(dataframe$month_name == "Mar")
-    Apr <- as.numeric(dataframe$month_name == "Apr")
-    May <- as.numeric(dataframe$month_name == "May")
-    Jun <- as.numeric(dataframe$month_name == "Jun")
-    Jul <- as.numeric(dataframe$month_name == "Jul")
-    Aug <- as.numeric(dataframe$month_name == "Aug")
-    Sep <- as.numeric(dataframe$month_name == "Sep")
-    Oct <- as.numeric(dataframe$month_name == "Oct")
-    Nov <- as.numeric(dataframe$month_name == "Nov")
-    Dec <- as.numeric(dataframe$month_name == "Dec")
-    # bind to the other_data_data
-    dataframe <- cbind(dataframe, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
-
-    # other_data_data columns needed
-
-    # lag each column
-    income_1 <- Lag(dataframe$income, 1)
-    hours_worked_1 <- Lag(dataframe$hours_worked, 1)
-    billable_hours_1 <- Lag(dataframe$billable_hours, 1)
-    billable_amount_1 <- Lag(dataframe$billable_amount, 1)
-    new_biz_hours_1 <- Lag(dataframe$new_biz_hours, 1)
-    new_biz_amount_1 <- Lag(dataframe$new_biz_amount, 1)
-    num_of_time_entries_1 <- Lag(dataframe$num_of_time_entries, 1)
-    number_of_time_entries_billable_1 <- Lag(dataframe$number_of_time_entries_billable, 1)
-    number_of_new_biz_time_entries_1 <- Lag(dataframe$number_of_new_biz_time_entries, 1)
-    cost_of_goods_sold_1 <- Lag(dataframe$cost_of_goods_sold, 1)
-    expense_1 <- Lag(dataframe$expense, 1)
-
-    # new other_data_data.frame of neural network other_data_data
-    NN.data <- data.frame(dataframe$income, dataframe$month, income_1, hours_worked_1, billable_hours_1, billable_amount_1,
-                          new_biz_hours_1, new_biz_amount_1, num_of_time_entries_1,
-                          number_of_time_entries_billable_1, number_of_new_biz_time_entries_1,
-                          cost_of_goods_sold_1, expense_1, Jan, Feb, Mar, Apr,
-                          May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
-
-
-    # omit nan
-    NN.data <- na.omit(NN.data)
-
-    samplesize <- test_percentage * nrow(NN.data)
-    set.seed(80)
-
-    index <- sample(seq_len(nrow(NN.data)), size = samplesize)
-    datatrain <- NN.data[index,]
-    datatest <- NN.data[-index,]
-    max <- apply(NN.data, 2, max)
-    min <- apply(NN.data, 2, min)
-    scaled <- as.data.frame(scale(NN.data, center = min, scale = max - min))
-    trainNN <- scaled[index,]
-    trainNN
-    testNN <- scaled[-index,]
-    set.seed(80)
-
-    NN <- neuralnet(dataframe.income ~ number_of_new_biz_time_entries_1 +
-      billable_amount_1 +
-      cost_of_goods_sold_1 +
-      dataframe.month +
-      Oct +
-      Nov +
-      Feb, trainNN, hidden = neuron_vec, linear.output = T, stepmax = 1e7)
-
-
-    # adjust to height of session
-    width <- session$clientData$output_im_plot9_width
-    height <- session$clientData$output_im_plot9_height
-
-    # A temp file to save the output. It will be deleted after renderImage
-    # sends it, because deleteFile=TRUE.
-    outfile <- tempfile(fileext = '.png')
-
-    png(outfile, width = width, height = height)
-    dev.off()
-    print(plot(NN))
-    dev.print(png, outfile,width = width, height = height)
-    dev.off()
-
-
-    list(src = outfile,
-         width = width,
-         height = height,
-         alt = "Rendered Neural Network")
+    render_neural_network(input$im_plot9_slider1,
+                          input$im_plot9_slider2,
+                          input$im_plot9_slider3,
+                          input$im_plot9_slider4)
 
 
   }, deleteFile = TRUE)
